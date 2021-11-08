1335ff07b54a7f44d5757cb3f99bd845695cd3c6
Shota Kubota <ta2@o2p.jp>
2021-11-08 19:36:03 +0900

    chore(textlint): add textlint config

---
+++ .textlintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    "@proofdict/proofdict": {
+      dictURL: "https://azu.github.io/proof-dictionary/",
+    },
+    "textlint-rule-preset-jtf-style": true,
+    "preset-jtf-style": {
+      "3.1.1.全角文字と半角文字の間": false,
+    },
+  },
+};
