673e272f9de55014dd124deb724167de63c13891
Shota Kubota <ta2@o2p.jp>
2022-01-22 21:27:41 +0900

    test(textlint): enabled textlint comment

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   rules: {
-    "@proofdict/proofdict": {
-      dictURL: "https://azu.github.io/proof-dictionary/",
+    '@proofdict/proofdict': {
+      dictURL: 'https://azu.github.io/proof-dictionary/',
     },
-    "textlint-rule-preset-jtf-style": true,
-    "preset-jtf-style": {
-      "3.1.1.全角文字と半角文字の間": false,
-    },
+    'textlint-rule-preset-jtf-style': true,
+  },
+  filters: {
+    comments: true,
   },
 };
