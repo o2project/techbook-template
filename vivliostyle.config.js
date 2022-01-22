b85d8e0f69c7cc680c4d012bb1be1159b08ae267
Shota Kubota <ta2@o2p.jp>
2022-01-22 21:17:27 +0900

    chore(vivliostyle): update vivliostyle config

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,33 +1,26 @@
 module.exports = {
-  title: "Vivliostyle Base", // populated into 'publication.json', default to 'title' of the first entry or 'name' in 'package.json'.
-  author: "kubosho", // default to 'author' in 'package.json' or undefined
+  title: "Vivliostyle Base",
+  author: "kubosho",
   language: "ja",
-  size: "B5",
-  // theme: '', // .css or local dir or npm package. default to undefined
+  size: "JIS-B5",
+  theme: '@o2project/vivliostyle-theme-o2project',
   entry: [
-    "./entries/introduction.md", // 'title' is automatically guessed from the file (frontmatter > first heading)
-    // {
-    //   path: "epigraph.md",
-    //   title: "おわりに", // title can be overwritten (entry > file),
-    //   theme: "@vivliostyle/theme-whatever", // theme can be set indivisually. default to root 'theme'
-    // },
-    // "glossary.html", // html is also acceptable
-  ], // 'entry' can be 'string' or 'object' if there's only single markdown file
-  //   entryContext: "./manuscripts", // default to '.' (relative to 'vivliostyle.config.js')
+    "./entries/introduction.md",
+  ],
   output: [
-    // path to generate draft file(s). default to '{title}.pdf'
-    "./out/pdf/book.pdf", // the output format will be inferred from the name.
+    {
+      path: "./out/pdf",
+      format: "pdf",
+    },
     {
       path: "./out/webpub",
       format: "webpub",
     },
   ],
-  workspaceDir: ".temp", // directory which is saved intermediate files.
-  // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
-  // cover: './cover.png', // cover image. default to undefined.
+  workspaceDir: ".temp",
+  toc: true,
+  tocTitle: '目次',
   vfm: {
-    // options of VFM processor
-    hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
-    //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
+    hardLineBreaks: true,
   },
 };
