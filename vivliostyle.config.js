e5fb3fc2b7cf9b516ad9033452879278142d8028
Shota Kubota <ta2@o2p.jp>
2024-09-21 21:46:55 +0900

    build(*): enable output in PDF/X-1a format

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,5 +1,17 @@
 const TITLE = 'Vivliostyle Base';
 
+const pdfConfig = {
+  path: `./dist/pdf/${TITLE}.pdf`,
+  format: 'pdf',
+};
+const webpubConfig = {
+  path: './dist/webpub/',
+  format: 'webpub',
+};
+
+const output =
+  process.env.PRESS_ONLY === 'true' ? [{ ...pdfConfig, path: `./dist/press/${TITLE}.pdf` }] : [pdfConfig, webpubConfig];
+
 module.exports = {
   title: TITLE,
   author: 'kubosho',
@@ -7,16 +19,7 @@ module.exports = {
   size: 'JIS-B5',
   theme: '@o2project/vivliostyle-theme-o2project',
   entry: ['./entries/introduction.md'],
-  output: [
-    {
-      path: `./dist/pdf/${TITLE}.pdf`,
-      format: 'pdf',
-    },
-    {
-      path: './dist/webpub/',
-      format: 'webpub',
-    },
-  ],
+  output,
   workspaceDir: '.temp',
   toc: {
     title: '目次',
